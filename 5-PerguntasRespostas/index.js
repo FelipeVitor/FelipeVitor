e7d1abe6971eb283345c84f8ec223720fdc4cb53
FelipeVitor <felipesv1900@hotmail.com>
2025-05-07 21:05:12 -0300

    save

---
+++ 5-PerguntasRespostas/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+
+
+app.get("/index",(req,res) => {
+    var nome = "Felipe";
+    var lang = "JavaScript";
+    var exibirMsg = false;
+
+    var listaProdutos = [
+        {nome: "Notebook", preco: 2500},
+        {nome: "Monitor", preco: 1500},
+        {nome: "Teclado", preco: 200},
+        {nome: "Mouse", preco: 100},
+    ]
+
+    res.render("index",{
+        nome: nome,
+        lang: lang,
+        empresa: "Seidor",
+        ano: 2025,
+        msg: exibirMsg,
+        produtos: listaProdutos
+    });
+});
+
+app.get("/perguntar",(req,res) => {
+
+    res.render("perguntar",{
+
+    });
+});
+
+app.listen(8080, ()=>{
+    console.log("App rodando!")
+});
